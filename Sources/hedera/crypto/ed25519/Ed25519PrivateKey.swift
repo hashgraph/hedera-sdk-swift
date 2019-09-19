07cca95b245d8039795723901423bc649cd17cad
Kristy Brambila <kristy@launchbadge.com>
2019-09-19 11:53:21 -0700

    WIP

--- Sources/hedera/crypto/ed25519/Ed25519PrivateKey.swift
+++ Sources/hedera/crypto/ed25519/Ed25519PrivateKey.swift
@@ -1,6 +1,8 @@
 import Sodium
 
 let ed25519PrivateKeyPrefix = "302e020100300506032b657004220420"
+let ed25519PrivateKeyLength = 32
+let combinedEd25519KeyLength = 64
 
 // TODO
 public struct Ed25519PrivateKey {
@@ -10,9 +12,29 @@ public struct Ed25519PrivateKey {
         inner = bytes
     }
 
-    public static func from(bytes: Bytes) -> Ed25519PrivateKey {
-        // TODO: check length first
-        return Ed25519PrivateKey(bytes: bytes)
+    public static func from(bytes: Bytes) -> Optional<Ed25519PrivateKey> {
+        if (bytes.count == ed25519PrivateKeyLength) {
+            return Ed25519PrivateKey(bytes: bytes)
+        } else if (bytes.count == combinedEd25519KeyLength) {
+            return Ed25519PrivateKey(bytes: Bytes(bytes.prefix(ed25519PrivateKeyLength)))
+        }
+        return nil
+    }
+
+    // Recover from a hex encoded string. Does not support key derivation.
+    public static func from(string: String) -> Optional<Ed25519PrivateKey> {
+        switch string.count {
+        case 64: // lone private key
+            fallthrough
+        case 128: // private key + public key
+            return from(bytes: hexDecode(string)!)
+        case 96:
+            if string.startsWith(ed25519PrivateKeyPrefix) {
+                return from(bytes: string.suffix(32))
+            }
+        default:
+            return nil    
+        }
     }
 
     public static func generate() -> Ed25519PrivateKey {
@@ -21,4 +43,18 @@ public struct Ed25519PrivateKey {
 
     // TODO
     // func getPublicKey() -> Ed25519PublicKey {}
+}
+
+func hexDecode(_ string: String) -> Optional<Bytes> {
+    if hex.length % 2 != 0 {
+        // error, it must be even length
+    }
+
+    let bytesLen = string.count / 2;
+    var bytes = Bytes(repeating: 0, count: bytesLen);
+    for i in 0...bytesLen {
+        bytes[i] = Int(string.slice(i*2, i*2+2), radix: 16)
+    }
+
+    return bytes
 }
--- Tests/hederaTests/crypto/ed25519/Ed25519PrivateKeyTests.swift
+++ Tests/hederaTests/crypto/ed25519/Ed25519PrivateKeyTests.swift
@@ -1,10 +1,12 @@
 import XCTest
+import Sodium
 import hedera
 
 final class Ed25519PrivateKeyTests: XCTestCase {
     let testKeyStr = "302e020100300506032b657004220420db484b828e64b2d8f12ce3c0a0e93a0b8cce7af1bb8f39c97732394482538e10";
     let testKeyPem = "-----BEGIN PRIVATE KEY-----\nMC4CAQAwBQYDK2VwBCIEINtIS4KOZLLY8SzjwKDpOguMznrxu485yXcyOUSCU44Q\n-----END PRIVATE KEY-----\n";
-    
+    let testKeyBytes = Bytes(arrayLiteral: -37, 72, 75, -126, -114, 100, -78, -40, -15, 44, -29, -64, -96, -23, 58, 11, -116, -50, 122, -15, -69, -113, 57, -55, 119, 50, 57, 68, -126, 83, -114, 16);
+
     func testGenerate() {
         XCTAssertNoThrow(Ed25519PrivateKey.generate())
     }
@@ -12,7 +14,7 @@ final class Ed25519PrivateKeyTests: XCTestCase {
     func testFromBytes() {
         let bytes = testKeyStr.bytes
         let key = Ed25519PrivateKey.from(bytes: bytes)
-        // assert they are correct
+        XCTAssertNotNil(key)
     }
 
     static var allTests = [
