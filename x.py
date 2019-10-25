2b8eb592b39687a4405dea15662684077c0ab81a
Daniel Akhterov <daniel@launchbadge.com>
2019-10-24 18:00:37 -0700

    Update x.py to build or test

--- x.py
+++ x.py
@@ -11,6 +11,21 @@ import subprocess
 import os
 from os import path
 
+# Useless if there aren't at least 2 arguments
+if len(sys.argv) < 2:
+    print("USAGE: x.py <operation>")
+    print("OPERATIONS: test | build")
+    sys.exit(1)
+
+# The #1 argument is always the <operation>
+operation = sys.argv[1]
+
+# Validate that this is an allowed operation
+OPERATIONS = ['test', 'build']
+if operation not in OPERATIONS:
+    print(f"target '{target}' not one of 'test' | 'build'")
+    sys.exit(0)
+
 # Check if we are inside the Docker environment
 if sys.argv[0] != '/opt/x.py':
     # Build the docker environment image
@@ -23,16 +38,17 @@ if sys.argv[0] != '/opt/x.py':
 
     # Re-run the script from within the docker environment
     pwd = os.getcwd()
-    out = subprocess.run(f'docker run --rm -v {pwd}:/workspace {image}', 
+    out = subprocess.run(f'docker run --rm -v {pwd}:/workspace {image} {operation}', 
         shell=True, check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     
     print(out.stdout.decode().strip())
 
     sys.exit(0)
-
-
 else:
-    subprocess.run(['swift', 'build'], check=True)
+    if operation == 'test':
+        subprocess.run(['swift', 'test'], check=True)
+    else:
+        subprocess.run(['swift', 'build'], check=True)
 
     # plugin = path.realpath('vendor/ledger-nanopb/generator/protoc-gen-nanopb')
     # plugin = f'--plugin=protoc-gen-nanopb={plugin}'
