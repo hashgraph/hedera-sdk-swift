b5e15aa5e1538dce9abf4b1f2295678d61aa0eca
Daniel Akhterov <daniel@launchbadge.com>
2019-10-30 16:36:23 -0700

    Add CreateFileExample

---
+++ Examples/CreateFile/main.swift
@@ -0,0 +1,18 @@
+import Hedera
+
+let privateKey = Ed25519PrivateKey("yourprivatekeyhere")!
+let op = Operator(id: AccountId(2), privateKey: privateKey)
+
+let client = Client(node: AccountId("0.0.3")!, address: "0.testnet.hedera.com:50211")
+    .setMaxTransactionFee(100_000_000)
+    .setOperator(op)
+
+let fileCreateTransactionId = try! FileCreateTransaction(client: client)
+    .addKey(privateKey.publicKey)
+    .setContents("This is a test")
+    .setMemo("File Create Example - Swift SDK")
+    .setTransactionFee(1_000_000_000)
+    .build()
+    .execute()
+
+print("File Create Example succeeded with transaction id \(fileCreateTransactionId)")
