f438e7443979891a605efc276959c2d213759c61
Kristy Brambila <kristy@launchbadge.com>
2019-11-01 18:32:11 -0700

    Make CreateAccountTransactionTest pass, even though it's not fully identical to Java

--- Sources/Hedera/account/AccountCreateTransaction.swift
+++ Sources/Hedera/account/AccountCreateTransaction.swift
@@ -10,10 +10,27 @@ public final class AccountCreateTransaction: TransactionBuilder {
         // Default to maximum values for record thresholds. Without this, records 
         // would be auto-created whenever a send or receive transaction takes place
         // for this new account. This should be an explicit ask.
-        inner.sendRecordThreshold = UInt64.max
-        inner.receiveRecordThreshold = UInt64.max
+        inner.sendRecordThreshold = UInt64(Int64.max)
+        inner.receiveRecordThreshold = UInt64(Int64.max)
 
         body.cryptoCreateAccount = inner
+    }
+    
+    @discardableResult
+    override public func setTransactionId(_ id: TransactionId) -> Self {
+        // Setting the transaction ID defaults the shard and realm IDs
+        // If you truly want to create a _new_ realm, then you need
+        // to null the realm after setting this
+
+        if (!body.cryptoCreateAccount.hasShardID) {
+            setShardId(id.accountId.id.shard);
+        }
+
+        if (!body.cryptoCreateAccount.hasRealmID) {
+            setRealmId(id.accountId.id.realm);
+        }
+
+        return super.setTransactionId(id) as! Self
     }
 
     @discardableResult
@@ -62,6 +79,24 @@ public final class AccountCreateTransaction: TransactionBuilder {
     public func setAutoRenewPeriod(_ period: TimeInterval) -> Self {
         body.cryptoCreateAccount.autoRenewPeriod = period.toProto()
 
+        return self
+    }
+    
+    @discardableResult
+    public func setShardId(_ id: UInt64) -> Self {
+        var shard = Proto_ShardID()
+        shard.shardNum = Int64(id)
+        body.cryptoCreateAccount.shardID = shard
+        
+        return self
+    }
+    
+    @discardableResult
+    public func setRealmId(_ id: UInt64) -> Self {
+        var realm = Proto_RealmID()
+        realm.realmNum = Int64(id)
+        body.cryptoCreateAccount.realmID = realm
+        
         return self
     }
     
