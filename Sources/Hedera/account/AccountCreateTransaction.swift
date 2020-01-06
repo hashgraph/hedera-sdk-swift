5e037992d8191c84e93578edf0ab0a1a17491c6d
Kristy Brambila <kristy@launchbadge.com>
2020-01-06 10:43:05 -0800

    Bring AccountCreateTransaction to parity

--- Sources/Hedera/account/AccountCreateTransaction.swift
+++ Sources/Hedera/account/AccountCreateTransaction.swift
@@ -17,62 +17,8 @@ public final class AccountCreateTransaction: TransactionBuilder {
     }
 
     @discardableResult
-    override public func setTransactionId(_ id: TransactionId) -> Self {
-        // Setting the transaction ID defaults the shard and realm IDs
-        // If you truly want to create a _new_ realm, then you need
-        // to null the realm after setting this
-
-        if !body.cryptoCreateAccount.hasShardID {
-            setShardId(id.accountId.id.shard)
-        }
-
-        if !body.cryptoCreateAccount.hasRealmID {
-            setRealmId(id.accountId.id.realm)
-        }
-
-        super.setTransactionId(id)
-
-        return self
-    }
-
-    @discardableResult
-    public func setKey(_ key: Ed25519PublicKey) -> Self {
+    public func setKey(_ key: PublicKey) -> Self {
         body.cryptoCreateAccount.key = key.toProto()
-
-        return self
-    }
-
-    @discardableResult
-    public func setInitialBalance(_ balance: UInt64) -> Self {
-        body.cryptoCreateAccount.initialBalance = balance
-
-        return self
-    }
-
-    @discardableResult
-    public func setProxyAccountId(_ id: AccountId) -> Self {
-        body.cryptoCreateAccount.proxyAccountID = id.toProto()
-
-        return self
-    }
-
-    @discardableResult
-    public func setSendRecordThreshold(_ threshold: UInt64) -> Self {
-        body.cryptoCreateAccount.sendRecordThreshold = threshold
-
-        return self
-    }
-
-    @discardableResult
-    public func setReceiveRecordThreshold(_ threshold: UInt64) -> Self {
-        body.cryptoCreateAccount.receiveRecordThreshold = threshold
-
-        return self
-    }
-
-    @discardableResult
-    public func setReceiverSignatureRequired(_ required: Bool) -> Self {
-        body.cryptoCreateAccount.receiverSigRequired = required
 
         return self
     }
@@ -85,19 +31,36 @@ public final class AccountCreateTransaction: TransactionBuilder {
     }
 
     @discardableResult
-    public func setShardId(_ id: UInt64) -> Self {
-        var shard = Proto_ShardID()
-        shard.shardNum = Int64(id)
-        body.cryptoCreateAccount.shardID = shard
+    public func setInitialBalance(_ balance: UInt64) -> Self {
+        body.cryptoCreateAccount.initialBalance = balance
 
         return self
     }
 
     @discardableResult
-    public func setRealmId(_ id: UInt64) -> Self {
-        var realm = Proto_RealmID()
-        realm.realmNum = Int64(id)
-        body.cryptoCreateAccount.realmID = realm
+    public func setReceiveRecordThreshold(_ threshold: UInt64) -> Self {
+        body.cryptoCreateAccount.receiveRecordThreshold = threshold
+
+        return self
+    }
+
+    @discardableResult
+    public func setSendRecordThreshold(_ threshold: UInt64) -> Self {
+        body.cryptoCreateAccount.sendRecordThreshold = threshold
+
+        return self
+    }
+
+    @discardableResult
+    public func setProxyAccountId(_ id: AccountId) -> Self {
+        body.cryptoCreateAccount.proxyAccountID = id.toProto()
+
+        return self
+    }
+
+    @discardableResult
+    public func setReceiverSignatureRequired(_ required: Bool) -> Self {
+        body.cryptoCreateAccount.receiverSigRequired = required
 
         return self
     }
