b70d3c2479168322d13e83edb60b52cbd383d663
Ryan Leckey <ryan@launchbadge.com>
2023-04-04 05:44:06 -0700

    release: 0.16.0
    Signed-off-by: Ryan Leckey <ryan@launchbadge.com>

--- Sources/Hedera/Schedule/AnySchedulableTransaction.swift
+++ Sources/Hedera/Schedule/AnySchedulableTransaction.swift
@@ -1,3 +1,25 @@
+/*
+ * ‌
+ * Hedera Swift SDK
+ * ​
+ * Copyright (C) 2022 - 2023 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+import HederaProtobufs
+
 internal enum AnySchedulableTransaction {
     case accountCreate(AccountCreateTransaction)
     case accountUpdate(AccountUpdateTransaction)
@@ -36,13 +58,14 @@ internal enum AnySchedulableTransaction {
     case systemUndelete(SystemUndeleteTransaction)
     case freeze(FreezeTransaction)
     case scheduleDelete(ScheduleDeleteTransaction)
+    case prng(PrngTransaction)
 
     internal init(upcasting transaction: Transaction) {
         self.init(AnyTransaction(upcasting: transaction))
     }
 
     // There's unfortunately very little that can be done about this.
-    // swiftlint:disable:next cyclomatic_complexity function_body_length
+    // swiftlint:disable:next function_body_length
     fileprivate init(_ anyTransaction: AnyTransaction) {
         switch anyTransaction {
         case .accountCreate(let transaction):
@@ -119,6 +142,8 @@ internal enum AnySchedulableTransaction {
             self = .freeze(transaction)
         case .scheduleDelete(let transaction):
             self = .scheduleDelete(transaction)
+        case .prng(let transaction):
+            self = .prng(transaction)
         case .ethereum:
             fatalError("Cannot schedule `EthereumTransaction`")
         case .scheduleCreate:
@@ -130,45 +155,45 @@ internal enum AnySchedulableTransaction {
 }
 
 extension AnySchedulableTransaction {
-    internal enum Kind: String, Codable {
-        case accountCreate
-        case accountUpdate
-        case accountDelete
-        case accountAllowanceApprove
-        case accountAllowanceDelete
-        case contractCreate
-        case contractUpdate
-        case contractDelete
-        case contractExecute
-        case transfer
-        case topicCreate
-        case topicUpdate
-        case topicDelete
-        case topicMessageSubmit
-        case fileAppend
-        case fileCreate
-        case fileUpdate
-        case fileDelete
-        case tokenAssociate
-        case tokenBurn
-        case tokenCreate
-        case tokenDelete
-        case tokenDissociate
-        case tokenFeeScheduleUpdate
-        case tokenFreeze
-        case tokenGrantKyc
-        case tokenMint
-        case tokenPause
-        case tokenRevokeKyc
-        case tokenUnfreeze
-        case tokenUnpause
-        case tokenUpdate
-        case tokenWipe
-        case systemDelete
-        case systemUndelete
-        case freeze
-        case scheduleDelete
-    }
+    // internal enum Kind: String {
+    //     case accountCreate
+    //     case accountUpdate
+    //     case accountDelete
+    //     case accountAllowanceApprove
+    //     case accountAllowanceDelete
+    //     case contractCreate
+    //     case contractUpdate
+    //     case contractDelete
+    //     case contractExecute
+    //     case transfer
+    //     case topicCreate
+    //     case topicUpdate
+    //     case topicDelete
+    //     case topicMessageSubmit
+    //     case fileAppend
+    //     case fileCreate
+    //     case fileUpdate
+    //     case fileDelete
+    //     case tokenAssociate
+    //     case tokenBurn
+    //     case tokenCreate
+    //     case tokenDelete
+    //     case tokenDissociate
+    //     case tokenFeeScheduleUpdate
+    //     case tokenFreeze
+    //     case tokenGrantKyc
+    //     case tokenMint
+    //     case tokenPause
+    //     case tokenRevokeKyc
+    //     case tokenUnfreeze
+    //     case tokenUnpause
+    //     case tokenUpdate
+    //     case tokenWipe
+    //     case systemDelete
+    //     case systemUndelete
+    //     case freeze
+    //     case scheduleDelete
+    // }
 
     internal var transaction: Transaction {
         switch self {
@@ -246,16 +271,54 @@ extension AnySchedulableTransaction {
             return transaction
         case .scheduleDelete(let transaction):
             return transaction
+        case .prng(let transaction):
+            return transaction
         }
     }
 }
 
-extension AnySchedulableTransaction: Codable {
-    internal init(from decoder: Decoder) throws {
-        self.init(try AnyTransaction(from: decoder))
+extension AnySchedulableTransaction: ToSchedulableTransactionData {
+    internal func toSchedulableTransactionData() -> Proto_SchedulableTransactionBody.OneOf_Data {
+        switch self {
+        case .accountCreate(let transaction): return transaction.toSchedulableTransactionData()
+        case .accountUpdate(let transaction): return transaction.toSchedulableTransactionData()
+        case .accountDelete(let transaction): return transaction.toSchedulableTransactionData()
+        case .accountAllowanceApprove(let transaction): return transaction.toSchedulableTransactionData()
+        case .accountAllowanceDelete(let transaction): return transaction.toSchedulableTransactionData()
+        case .contractCreate(let transaction): return transaction.toSchedulableTransactionData()
+        case .contractUpdate(let transaction): return transaction.toSchedulableTransactionData()
+        case .contractDelete(let transaction): return transaction.toSchedulableTransactionData()
+        case .contractExecute(let transaction): return transaction.toSchedulableTransactionData()
+        case .transfer(let transaction): return transaction.toSchedulableTransactionData()
+        case .topicCreate(let transaction): return transaction.toSchedulableTransactionData()
+        case .topicUpdate(let transaction): return transaction.toSchedulableTransactionData()
+        case .topicDelete(let transaction): return transaction.toSchedulableTransactionData()
+        case .topicMessageSubmit(let transaction): return transaction.toSchedulableTransactionData()
+        case .fileAppend(let transaction): return transaction.toSchedulableTransactionData()
+        case .fileCreate(let transaction): return transaction.toSchedulableTransactionData()
+        case .fileUpdate(let transaction): return transaction.toSchedulableTransactionData()
+        case .fileDelete(let transaction): return transaction.toSchedulableTransactionData()
+        case .tokenAssociate(let transaction): return transaction.toSchedulableTransactionData()
+        case .tokenBurn(let transaction): return transaction.toSchedulableTransactionData()
+        case .tokenCreate(let transaction): return transaction.toSchedulableTransactionData()
+        case .tokenDelete(let transaction): return transaction.toSchedulableTransactionData()
+        case .tokenDissociate(let transaction): return transaction.toSchedulableTransactionData()
+        case .tokenFeeScheduleUpdate(let transaction): return transaction.toSchedulableTransactionData()
+        case .tokenFreeze(let transaction): return transaction.toSchedulableTransactionData()
+        case .tokenGrantKyc(let transaction): return transaction.toSchedulableTransactionData()
+        case .tokenMint(let transaction): return transaction.toSchedulableTransactionData()
+        case .tokenPause(let transaction): return transaction.toSchedulableTransactionData()
+        case .tokenRevokeKyc(let transaction): return transaction.toSchedulableTransactionData()
+        case .tokenUnfreeze(let transaction): return transaction.toSchedulableTransactionData()
+        case .tokenUnpause(let transaction): return transaction.toSchedulableTransactionData()
+        case .tokenUpdate(let transaction): return transaction.toSchedulableTransactionData()
+        case .tokenWipe(let transaction): return transaction.toSchedulableTransactionData()
+        case .systemDelete(let transaction): return transaction.toSchedulableTransactionData()
+        case .systemUndelete(let transaction): return transaction.toSchedulableTransactionData()
+        case .freeze(let transaction): return transaction.toSchedulableTransactionData()
+        case .scheduleDelete(let transaction): return transaction.toSchedulableTransactionData()
+        case .prng(let transaction): return transaction.toSchedulableTransactionData()
+        }
     }
 
-    internal func encode(to encoder: Encoder) throws {
-        try transaction.encode(to: encoder)
-    }
 }
--- Sources/Hedera/TransactionRecord.swift
+++ Sources/Hedera/TransactionRecord.swift
@@ -19,11 +19,11 @@
  */
 
 import Foundation
+import HederaProtobufs
 
-// TODO: assessed_custom_fees
 /// The complete record for a transaction on Hedera that has reached consensus.
 /// Response from `TransactionRecordQuery`.
-public struct TransactionRecord: Codable {
+public struct TransactionRecord {
     /// The status (reach consensus, or failed, or is unknown) and the ID of
     /// any new account/file/instance created.
     public let receipt: TransactionReceipt
@@ -86,34 +86,107 @@ public struct TransactionRecord: Codable {
 
     /// The keccak256 hash of the ethereumData. This field will only be populated for
     /// `EthereumTransaction`.
-    public let ethereumHash: Data?
+    public let ethereumHash: Data
 
     /// The last 20 bytes of the keccak-256 hash of a ECDSA_SECP256K1 primitive key.
     public let evmAddress: EvmAddress?
 
-    public init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
+    /// In the record of a PRNG transaction with no output range, a pseudorandom 384-bit string.
+    public let prngBytes: Data?
 
-        receipt = try container.decode(.receipt)
-        transactionHash = try container.decodeIfPresent(.transactionHash).map(Data.base64Encoded) ?? Data()
-        consensusTimestamp = try container.decode(.consensusTimestamp)
-        contractFunctionResult = try container.decodeIfPresent(.contractFunctionResult)
-        transfers = try container.decode(.transfers)
-        tokenTransfers = try container.decode(.tokenTransfers)
-        tokenNftTransfers = try container.decode(.tokenNftTransfers)
-        transactionId = try container.decode(.transactionId)
-        transactionMemo = try container.decode(.transactionMemo)
-        transactionFee = try container.decode(.transactionFee)
-        scheduleRef = try container.decodeIfPresent(.scheduleRef)
-        assessedCustomFees = try container.decode(.assessedCustomFees)
-        automaticTokenAssociations = try container.decode(.automaticTokenAssociations)
-        evmAddress = try container.decode(.evmAddress)
+    /// In the record of a PRNG transaction with an output range, the output of a PRNG
+    /// whose input was a 384-bit string.
+    public let prngNumber: UInt32?
+}
 
-        parentConsensusTimestamp = try container.decodeIfPresent(.parentConsensusTimestamp)
+extension TransactionRecord {
+    internal static func fromProtobuf(_ proto: Proto_TransactionGetRecordResponse) throws -> Self {
+        return try Self(
+            protobuf: proto.transactionRecord,
+            duplicates: .fromProtobuf(proto.duplicateTransactionRecords),
+            children: .fromProtobuf(proto.childTransactionRecords)
+        )
+    }
 
-        aliasKey = try container.decodeIfPresent(.aliasKey)
-        children = try container.decodeIfPresent(.children) ?? []
-        duplicates = try container.decodeIfPresent(.duplicates) ?? []
-        ethereumHash = try container.decodeIfPresent(.ethereumHash).map(Data.base64Encoded) ?? Data()
+    fileprivate init(protobuf proto: Protobuf, duplicates: [Self], children: [Self]) throws {
+        let contractFunctionResult = try proto.body.map { body -> ContractFunctionResult in
+            switch body {
+            case .contractCallResult(let result), .contractCreateResult(let result):
+                return try ContractFunctionResult.fromProtobuf(result)
+            }
+        }
+
+        var tokenTransfers: [TokenId: [AccountId: Int64]] = [:]
+        var tokenNftTransfers: [TokenId: [TokenNftTransfer]] = [:]
+
+        for transfer in proto.tokenTransferLists {
+            let tokenId = TokenId.fromProtobuf(transfer.token)
+
+            var innerTokenTransfers = tokenTransfers[tokenId] ?? [:]
+
+            for accountAmount in transfer.transfers {
+                let accountId = try AccountId.fromProtobuf(accountAmount.accountID)
+                innerTokenTransfers[accountId] = accountAmount.amount
+            }
+
+            var nftTransfers = tokenNftTransfers[tokenId] ?? []
+
+            nftTransfers.append(
+                contentsOf: try transfer.nftTransfers.map { try TokenNftTransfer.fromProtobuf($0, tokenId: tokenId) }
+            )
+
+            tokenNftTransfers[tokenId] = nftTransfers
+
+            tokenTransfers[tokenId] = innerTokenTransfers
+        }
+
+        let evmAddress = !proto.evmAddress.isEmpty ? try EvmAddress(proto.evmAddress) : nil
+
+        let prngBytes: Data?
+        let prngNumber: UInt32?
+
+        switch proto.entropy {
+        case .prngBytes(let data):
+            prngBytes = data
+            prngNumber = nil
+        case .prngNumber(let number):
+            prngBytes = nil
+            prngNumber = UInt32(bitPattern: number)
+        case nil:
+            prngBytes = nil
+            prngNumber = nil
+        }
+
+        self.init(
+            receipt: try .fromProtobuf(proto.receipt),
+            transactionHash: proto.transactionHash,
+            consensusTimestamp: .fromProtobuf(proto.consensusTimestamp),
+            contractFunctionResult: contractFunctionResult,
+            transfers: try .fromProtobuf(proto.transferList.accountAmounts),
+            tokenTransfers: tokenTransfers,
+            tokenNftTransfers: tokenNftTransfers,
+            transactionId: try .fromProtobuf(proto.transactionID),
+            transactionMemo: proto.memo,
+            transactionFee: .fromTinybars(Int64(proto.transactionFee)),
+            scheduleRef: proto.hasScheduleRef ? .fromProtobuf(proto.scheduleRef) : nil,
+            assessedCustomFees: try .fromProtobuf(proto.assessedCustomFees),
+            automaticTokenAssociations: try .fromProtobuf(proto.automaticTokenAssociations),
+            parentConsensusTimestamp: .fromProtobuf(proto.parentConsensusTimestamp),
+            aliasKey: try .fromAliasBytes(proto.alias),
+            children: children,
+            duplicates: duplicates,
+            ethereumHash: proto.ethereumHash,
+            evmAddress: evmAddress,
+            prngBytes: prngBytes,
+            prngNumber: prngNumber
+        )
+    }
+}
+
+extension TransactionRecord: TryFromProtobuf {
+    internal typealias Protobuf = Proto_TransactionRecord
+
+    internal init(protobuf proto: Protobuf) throws {
+        try self.init(protobuf: proto, duplicates: [], children: [])
     }
 }
