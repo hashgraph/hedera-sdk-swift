a930b663dbb054357e63a35b52042a7b14314e36
Skyler Ross <skyler@launchbadge.com>
2023-06-07 13:30:00 -0700

    test(e2e): AccountAllowanceApprove
    Signed-off-by: Skyler Ross <skyler@launchbadge.com>

---
+++ Tests/HederaE2ETests/AccountAllowanceApprove.swift
@@ -0,0 +1,59 @@
+/*
+ * ‌
+ * Hedera Swift SDK
+ * ​
+ * Copyright (C) 2022 - 2023 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+import Hedera
+import XCTest
+
+internal final class AccountAllowanceApprove: XCTestCase {
+    internal func createAccount(_ testEnv: NonfreeTestEnvironment, balance: Hbar) async throws -> Account {
+        let account = try await Account.create(testEnv, balance: balance)
+
+        addTeardownBlock {
+            try await account.delete(testEnv)
+        }
+
+        return account
+    }
+
+    internal func testSpend() async throws {
+        let testEnv = try TestEnvironment.nonFree
+
+        let (alice, bob) = try await (createAccount(testEnv, balance: 10), createAccount(testEnv, balance: 10))
+
+        _ = try await AccountAllowanceApproveTransaction()
+            .approveHbarAllowance(bob.id, alice.id, 10)
+            .freezeWith(testEnv.client)
+            .sign(bob.key)
+            .execute(testEnv.client)
+            .getReceipt(testEnv.client)
+
+        let transferRecord = try await TransferTransaction()
+            .hbarTransfer(testEnv.operator.accountId, 5)
+            .approvedHbarTransfer(bob.id, -5)
+            .transactionId(TransactionId.generateFrom(alice.id))
+            .freezeWith(testEnv.client)
+            .sign(alice.key)
+            .execute(testEnv.client)
+            .getRecord(testEnv.client)
+
+        let transfer = try XCTUnwrap(transferRecord.transfers.first { $0.accountId == testEnv.operator.accountId })
+        XCTAssertEqual(transfer.amount, 5)
+    }
+}
