132e663b52026d2ab73b04ebb54620b0887dae92
Skyler Ross <skyler@launchbadge.com>
2023-08-23 13:25:00 -0700

    test(unit): TokenDissociateTransaction
    Signed-off-by: Skyler Ross <skyler@launchbadge.com>

---
+++ Tests/HederaTests/TokenDissociateTransactionTests.swift
@@ -0,0 +1,95 @@
+/*
+ * ‌
+ * Hedera Swift SDK
+ * ​
+ * Copyright (C) 2022 - 2023 Hedera Hashgraph, LLC
+ * ​
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ‍
+ */
+
+import HederaProtobufs
+import SnapshotTesting
+import XCTest
+
+@testable import Hedera
+
+internal final class TokenDissociateTransactionTests: XCTestCase {
+    internal static let testAccountId: AccountId = "6.9.0"
+
+    internal static let testTokenIds: [TokenId] = ["4.2.0", "4.2.1", "4.2.2"]
+
+    internal static let testTxId: TransactionId = TransactionId(
+        accountId: 5006,
+        validStart: Timestamp(seconds: 1_554_158_542, subSecondNanos: 0),
+        scheduled: false
+    )
+
+    internal static let unusedPrivateKey: PrivateKey =
+        "302e020100300506032b657004220420db484b828e64b2d8f12ce3c0a0e93a0b8cce7af1bb8f39c97732394482538e10"
+
+    private static func makeTransaction() throws -> TokenDissociateTransaction {
+        try TokenDissociateTransaction()
+            .nodeAccountIds([AccountId("0.0.5005"), AccountId("0.0.5006")])
+            .transactionId(testTxId)
+            .tokenIds(testTokenIds)
+            .accountId(testAccountId)
+            .freeze()
+            .sign(unusedPrivateKey)
+    }
+
+    internal func testSerialize() throws {
+        let tx = try Self.makeTransaction().makeProtoBody()
+
+        assertSnapshot(matching: tx, as: .description)
+    }
+
+    internal func testToFromBytes() throws {
+        let tx = try Self.makeTransaction()
+
+        let tx2 = try Transaction.fromBytes(tx.toBytes())
+
+        XCTAssertEqual(try tx.makeProtoBody(), try tx2.makeProtoBody())
+    }
+
+    internal func testFromProtoBody() throws {
+        let protoData = Proto_TokenDissociateTransactionBody.with { proto in
+            proto.account = Self.testAccountId.toProtobuf()
+            proto.tokens = Self.testTokenIds.toProtobuf()
+        }
+
+        let protoBody = Proto_TransactionBody.with { proto in
+            proto.tokenDissociate = protoData
+            proto.transactionID = Self.testTxId.toProtobuf()
+        }
+
+        let tx = try TokenDissociateTransaction(protobuf: protoBody, protoData)
+
+        XCTAssertEqual(tx.accountId, Self.testAccountId)
+        XCTAssertEqual(tx.tokenIds, Self.testTokenIds)
+    }
+
+    internal func testGetSetAccountId() {
+        let tx = TokenDissociateTransaction()
+        tx.accountId(Self.testAccountId)
+
+        XCTAssertEqual(tx.accountId, Self.testAccountId)
+    }
+
+    internal func testGetSetTokenIds() {
+        let tx = TokenDissociateTransaction()
+        tx.tokenIds(Self.testTokenIds)
+
+        XCTAssertEqual(tx.tokenIds, Self.testTokenIds)
+    }
+}
