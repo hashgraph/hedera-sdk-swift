5964985172e2f8da2a3fc69e88d8597198749a5c
Skyler Ross <skyler@launchbadge.com>
2023-08-28 16:37:11 -0700

    test(unit): Add a couple extra tests to TokenFreezeTransaction
    Following on a pattern established by some newer tests
    
    Signed-off-by: Skyler Ross <skyler@launchbadge.com>

--- Tests/HederaTests/TokenFreezeTransactionTests.swift
+++ Tests/HederaTests/TokenFreezeTransactionTests.swift
@@ -33,12 +33,15 @@ internal final class TokenFreezeTransactionTests: XCTestCase {
     internal static let unusedPrivateKey: PrivateKey =
         "302e020100300506032b657004220420db484b828e64b2d8f12ce3c0a0e93a0b8cce7af1bb8f39c97732394482538e10"
 
+    private static let testAccountId: AccountId = 222
+    private static let testTokenId: TokenId = "6.5.4"
+
     private static func makeTransaction() throws -> TokenFreezeTransaction {
         try TokenFreezeTransaction()
             .nodeAccountIds([5005, 5006])
             .transactionId(testTxId)
-            .accountId(222)
-            .tokenId("6.5.4")
+            .accountId(testAccountId)
+            .tokenId(testTokenId)
             .freeze()
             .sign(unusedPrivateKey)
     }
@@ -55,5 +58,36 @@ internal final class TokenFreezeTransactionTests: XCTestCase {
         let tx2 = try Transaction.fromBytes(tx.toBytes())
 
         XCTAssertEqual(try tx.makeProtoBody(), try tx2.makeProtoBody())
+    }
+
+    internal func testFromProtoBody() throws {
+        let protoData = Proto_TokenFreezeAccountTransactionBody.with { proto in
+            proto.account = Self.testAccountId.toProtobuf()
+            proto.token = Self.testTokenId.toProtobuf()
+        }
+
+        let protoBody = Proto_TransactionBody.with { proto in
+            proto.tokenFreeze = protoData
+            proto.transactionID = Self.testTxId.toProtobuf()
+        }
+
+        let tx = try TokenFreezeTransaction(protobuf: protoBody, protoData)
+
+        XCTAssertEqual(tx.accountId, Self.testAccountId)
+        XCTAssertEqual(tx.tokenId, Self.testTokenId)
+    }
+
+    internal func testGetSetAccountId() {
+        let tx = TokenFreezeTransaction()
+        tx.accountId(Self.testAccountId)
+
+        XCTAssertEqual(tx.accountId, Self.testAccountId)
+    }
+
+    internal func testGetSetTokenId() {
+        let tx = TokenFreezeTransaction()
+        tx.tokenId(Self.testTokenId)
+
+        XCTAssertEqual(tx.tokenId, Self.testTokenId)
     }
 }
