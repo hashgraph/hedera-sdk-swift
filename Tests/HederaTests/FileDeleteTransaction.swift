a3ee2195aeee30373ebdcad5654d15fda55ca828
Skyler Ross <skyler@launchbadge.com>
2023-08-29 15:37:44 -0700

    test(unit): add more FileDeleteTransactionTests
    Signed-off-by: Skyler Ross <skyler@launchbadge.com>

--- Tests/HederaTests/FileDeleteTransaction.swift
+++ Tests/HederaTests/FileDeleteTransaction.swift
@@ -28,15 +28,15 @@ internal final class FileDeleteTransactionTests: XCTestCase {
     internal static let unusedPrivateKey: PrivateKey =
         "302e020100300506032b657004220420db484b828e64b2d8f12ce3c0a0e93a0b8cce7af1bb8f39c97732394482538e10"
 
+    internal static let testTxId: TransactionId = TransactionId(
+        accountId: 5006,
+        validStart: Timestamp(seconds: 1_554_158_542, subSecondNanos: 0)
+    )
+
     private static func makeTransaction() throws -> FileDeleteTransaction {
         try FileDeleteTransaction()
             .nodeAccountIds([5005, 5006])
-            .transactionId(
-                TransactionId(
-                    accountId: 5006,
-                    validStart: Timestamp(seconds: 1_554_158_542, subSecondNanos: 0)
-                )
-            )
+            .transactionId(testTxId)
             .fileId("0.0.6006")
             .maxTransactionFee(.fromTinybars(100_000))
             .freeze()
@@ -54,5 +54,28 @@ internal final class FileDeleteTransactionTests: XCTestCase {
         let tx2 = try Transaction.fromBytes(tx.toBytes())
 
         XCTAssertEqual(try tx.makeProtoBody(), try tx2.makeProtoBody())
+    }
+
+    internal func testFromProtoBody() throws {
+        let protoData = Proto_FileDeleteTransactionBody.with { proto in
+            proto.fileID = FileId(num: 6006).toProtobuf()
+        }
+
+        let protoBody = Proto_TransactionBody.with { proto in
+            proto.fileDelete = protoData
+            proto.transactionID = Self.testTxId.toProtobuf()
+        }
+
+        let tx = try FileDeleteTransaction(protobuf: protoBody, protoData)
+
+        XCTAssertEqual(tx.fileId, 6006)
+    }
+
+    internal func testGetSetFileId() {
+        let tx = FileDeleteTransaction()
+
+        tx.fileId(1234)
+
+        XCTAssertEqual(tx.fileId, 1234)
     }
 }
