fe355642f0d4d36957a4493220721630f409c2ca
Skyler Ross <skyler@launchbadge.com>
2023-08-30 10:58:01 -0700

    test(unit): More EthereumTransaction tests
    Signed-off-by: Skyler Ross <skyler@launchbadge.com>

--- Tests/HederaTests/EthereumTransactionTests.swift
+++ Tests/HederaTests/EthereumTransactionTests.swift
@@ -25,14 +25,18 @@ import XCTest
 @testable import Hedera
 
 internal final class EthereumTransactionTests: XCTestCase {
+    private static let ethereumData = "livestock".data(using: .utf8)!
+    private static let callDataFileId: FileId = "4.5.6"
+    private static let maxGasAllowanceHbar: Hbar = 3
+
     private static func createTransaction() throws -> EthereumTransaction {
         try EthereumTransaction()
             .nodeAccountIds(Resources.nodeAccountIds)
             .transactionId(Resources.txId)
             .sign(Resources.privateKey)
-            .ethereumData("livestock".data(using: .utf8)!)
-            .callDataFileId(FileId.fromString("4.5.6"))
-            .maxGasAllowanceHbar(Hbar.fromString("3"))
+            .ethereumData(ethereumData)
+            .callDataFileId(callDataFileId)
+            .maxGasAllowanceHbar(maxGasAllowanceHbar)
             .maxTransactionFee(Hbar(1))
             .freeze()
     }
@@ -46,5 +50,43 @@ internal final class EthereumTransactionTests: XCTestCase {
         let tx2 = try Transaction.fromBytes(tx.toBytes())
 
         XCTAssertEqual(try tx.makeProtoBody(), try tx2.makeProtoBody())
+    }
+
+    internal func testFromProtoBody() throws {
+        let protoData = Proto_EthereumTransactionBody.with { proto in
+            proto.ethereumData = Self.ethereumData
+            proto.callData = Self.callDataFileId.toProtobuf()
+            proto.maxGasAllowance = Self.maxGasAllowanceHbar.tinybars
+        }
+
+        let protoBody = Proto_TransactionBody.with { proto in
+            proto.ethereumTransaction = protoData
+            proto.transactionID = Resources.txId.toProtobuf()
+        }
+
+        let tx = try EthereumTransaction(protobuf: protoBody, protoData)
+
+        XCTAssertEqual(tx.ethereumData, Self.ethereumData)
+        XCTAssertEqual(tx.callDataFileId, Self.callDataFileId)
+        XCTAssertEqual(tx.maxGasAllowanceHbar, Self.maxGasAllowanceHbar)
+    }
+
+    internal func testGetSetEthereumData() {
+        let tx = EthereumTransaction()
+        tx.ethereumData(Self.ethereumData)
+
+        XCTAssertEqual(tx.ethereumData, Self.ethereumData)
+    }
+    internal func testGetSetCallDataFileId() {
+        let tx = EthereumTransaction()
+        tx.callDataFileId(Self.callDataFileId)
+
+        XCTAssertEqual(tx.callDataFileId, Self.callDataFileId)
+    }
+    internal func testGetSetMaxGasAllowanceHbar() {
+        let tx = EthereumTransaction()
+        tx.maxGasAllowanceHbar(Self.maxGasAllowanceHbar)
+
+        XCTAssertEqual(tx.maxGasAllowanceHbar, Self.maxGasAllowanceHbar)
     }
 }
