ba70db1fb640ee3c141541712e331e45d0f826d8
Skyler Ross <skyler@launchbadge.com>
2023-08-29 17:14:27 -0700

    test(unit): more AccountUpdateTransaction tests
    Signed-off-by: Skyler Ross <skyler@launchbadge.com>

--- Tests/HederaTests/AccountUpdateTransactionTests.swift
+++ Tests/HederaTests/AccountUpdateTransactionTests.swift
@@ -25,6 +25,14 @@ import XCTest
 @testable import Hedera
 
 internal class AccountUpdateTransactionTests: XCTestCase {
+    private static let accountId: AccountId = 2002
+    private static let autoRenewPeriod: Duration = .hours(10)
+    private static let expirationTime = Timestamp(seconds: 1_554_158_543, subSecondNanos: 0)
+    private static let receiverSignatureRequired = false
+    private static let accountMemo = "Some memo"
+    private static let stakedAccountId: AccountId = "0.0.3"
+    private static let stakedNodeId: UInt64 = 4
+
     private static func makeTransaction() throws -> AccountUpdateTransaction {
         let tx = AccountUpdateTransaction()
 
@@ -35,13 +43,13 @@ internal class AccountUpdateTransactionTests: XCTestCase {
             .nodeAccountIds(Resources.nodeAccountIds)
             .transactionId(Resources.txId)
             .sign(Resources.privateKey)
-            .accountId(2002)
-            .autoRenewPeriod(.hours(10))
-            .expirationTime(Timestamp(seconds: 1_554_158_543, subSecondNanos: 0))
-            .receiverSignatureRequired(false)
-            .accountMemo("Some memo")
+            .accountId(accountId)
+            .autoRenewPeriod(autoRenewPeriod)
+            .expirationTime(expirationTime)
+            .receiverSignatureRequired(receiverSignatureRequired)
+            .accountMemo(accountMemo)
             .maxTransactionFee(.fromTinybars(100_000))
-            .stakedAccountId("0.0.3")
+            .stakedAccountId(stakedAccountId)
             .freeze()
     }
 
@@ -55,13 +63,13 @@ internal class AccountUpdateTransactionTests: XCTestCase {
             .nodeAccountIds(Resources.nodeAccountIds)
             .transactionId(Resources.txId)
             .sign(Resources.privateKey)
-            .accountId(2002)
-            .autoRenewPeriod(.hours(10))
-            .expirationTime(Timestamp(seconds: 1_554_158_543, subSecondNanos: 0))
-            .receiverSignatureRequired(false)
-            .accountMemo("Some memo")
+            .accountId(accountId)
+            .autoRenewPeriod(autoRenewPeriod)
+            .expirationTime(expirationTime)
+            .receiverSignatureRequired(receiverSignatureRequired)
+            .accountMemo(accountMemo)
             .maxTransactionFee(.fromTinybars(100_000))
-            .stakedNodeId(4)
+            .stakedNodeId(stakedNodeId)
             .freeze()
     }
 
@@ -89,5 +97,80 @@ internal class AccountUpdateTransactionTests: XCTestCase {
         let tx2 = try Transaction.fromBytes(tx.toBytes())
 
         XCTAssertEqual(try tx.makeProtoBody(), try tx2.makeProtoBody())
+    }
+
+    internal func testFromProtoBody() throws {
+        let protoData = Proto_CryptoUpdateTransactionBody.with { proto in
+            proto.accountIdtoUpdate = Self.accountId.toProtobuf()
+            proto.autoRenewPeriod = Self.autoRenewPeriod.toProtobuf()
+            proto.expirationTime = Self.expirationTime.toProtobuf()
+            proto.receiverSigRequiredWrapper = .init(Self.receiverSignatureRequired)
+            proto.memo = .init(Self.accountMemo)
+            proto.stakedAccountID = Self.stakedAccountId.toProtobuf()
+        }
+
+        let protoBody = Proto_TransactionBody.with { proto in
+            proto.cryptoUpdateAccount = protoData
+            proto.transactionID = Resources.txId.toProtobuf()
+        }
+
+        let tx = try AccountUpdateTransaction(protobuf: protoBody, protoData)
+
+        XCTAssertEqual(tx.accountId, Self.accountId)
+        XCTAssertEqual(tx.autoRenewPeriod, Self.autoRenewPeriod)
+        XCTAssertEqual(tx.expirationTime, Self.expirationTime)
+        XCTAssertEqual(tx.receiverSignatureRequired, Self.receiverSignatureRequired)
+        XCTAssertEqual(tx.accountMemo, Self.accountMemo)
+        XCTAssertEqual(tx.stakedAccountId, Self.stakedAccountId)
+        XCTAssertEqual(tx.stakedNodeId, nil)
+    }
+
+    internal func testGetSetAccountId() {
+        let tx = AccountUpdateTransaction()
+        tx.accountId(Self.accountId)
+
+        XCTAssertEqual(tx.accountId, Self.accountId)
+    }
+
+    internal func testGetSetAutoRenewPeriod() {
+        let tx = AccountUpdateTransaction()
+        tx.autoRenewPeriod(Self.autoRenewPeriod)
+
+        XCTAssertEqual(tx.autoRenewPeriod, Self.autoRenewPeriod)
+    }
+
+    internal func testGetSetExpirationTime() {
+        let tx = AccountUpdateTransaction()
+        tx.expirationTime(Self.expirationTime)
+
+        XCTAssertEqual(tx.expirationTime, Self.expirationTime)
+    }
+
+    internal func testGetSetReceiverSignatureRequired() {
+        let tx = AccountUpdateTransaction()
+        tx.receiverSignatureRequired(Self.receiverSignatureRequired)
+
+        XCTAssertEqual(tx.receiverSignatureRequired, Self.receiverSignatureRequired)
+    }
+
+    internal func testGetSetAccountMemo() {
+        let tx = AccountUpdateTransaction()
+        tx.accountMemo(Self.accountMemo)
+
+        XCTAssertEqual(tx.accountMemo, Self.accountMemo)
+    }
+
+    internal func testGetSetStakedAccountId() {
+        let tx = AccountUpdateTransaction()
+        tx.stakedAccountId(Self.stakedAccountId)
+
+        XCTAssertEqual(tx.stakedAccountId, Self.stakedAccountId)
+    }
+
+    internal func testGetSetStakedNodeId() {
+        let tx = AccountUpdateTransaction()
+        tx.stakedNodeId(Self.stakedNodeId)
+
+        XCTAssertEqual(tx.stakedNodeId, Self.stakedNodeId)
     }
 }
