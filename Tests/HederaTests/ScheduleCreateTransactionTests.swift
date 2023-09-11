5de810ac053d3c944b10f4f5a0942e79505def59
Ricky Saechao <ricky@launchbadge.com>
2023-09-10 17:06:07 -0700

    test(unit): more test ScheduleCreateTransaction
    Signed-off-by: Ricky Saechao <ricky@launchbadge.com>

--- Tests/HederaTests/ScheduleCreateTransactionTests.swift
+++ Tests/HederaTests/ScheduleCreateTransactionTests.swift
@@ -28,6 +28,8 @@ internal final class ScheduleCreateTransactionTests: XCTestCase {
     internal static let unusedPrivateKey: PrivateKey =
         "302e020100300506032b657004220420db484b828e64b2d8f12ce3c0a0e93a0b8cce7af1bb8f39c97732394482538e10"
 
+    internal static let testMemo = "test memo"
+
     private static func makeTransaction() throws -> ScheduleCreateTransaction {
         let transferTx = try TransferTransaction()
             .hbarTransfer(AccountId.fromString("0.0.555"), -10)
@@ -39,7 +41,7 @@ internal final class ScheduleCreateTransactionTests: XCTestCase {
                 TransactionId(
                     accountId: 5006, validStart: Timestamp(seconds: 1_554_158_542, subSecondNanos: 0), scheduled: false)
             )
-            .adminKey(.single(unusedPrivateKey.publicKey))
+            .adminKey(.single(Resources.publicKey))
             .payerAccountId(AccountId.fromString("0.0.222"))
             .scheduleMemo("flook")
             .maxTransactionFee(1)
@@ -58,8 +60,58 @@ internal final class ScheduleCreateTransactionTests: XCTestCase {
         let tx = try Self.makeTransaction()
         let tx2 = try Transaction.fromBytes(tx.toBytes())
 
-        print(try tx.makeProtoBody())
-
         XCTAssertEqual(try tx.makeProtoBody(), try tx2.makeProtoBody())
     }
+
+    internal func testFromProtoBody() throws{
+        let protoData = Proto_ScheduleCreateTransactionBody.with {proto in
+            proto.adminKey = Key.single(Resources.publicKey).toProtobuf()
+            proto.expirationTime = Resources.validStart.toProtobuf()
+            proto.payerAccountID = Resources.accountId.toProtobuf()
+            proto.memo = "Flook"
+            proto.waitForExpiry = true
+        }
+
+        let protoBody = Proto_TransactionBody.with {proto in
+            proto.scheduleCreate = protoData
+            proto.transactionID = Resources.txId.toProtobuf()
+        }
+
+        let tx = try ScheduleCreateTransaction(protobuf: protoBody, protoData)
+
+        XCTAssertEqual(tx.adminKey, Key.single(Resources.publicKey))
+        XCTAssertEqual(tx.expirationTime, Resources.validStart)
+        XCTAssertEqual(tx.payerAccountId?.num, Resources.accountId.num)
+        XCTAssertEqual(tx.scheduleMemo, "Flook")
+        XCTAssertEqual(tx.isWaitForExpiry, true)
+    }
+
+    internal func testGetSetPayerAccountId() throws {
+        let tx = ScheduleCreateTransaction()
+        tx.payerAccountId(Resources.accountId)
+
+        XCTAssertEqual(tx.payerAccountId, Resources.accountId)
+    }
+
+    internal func testGetSetAdminKey() throws {
+        let tx = ScheduleCreateTransaction()
+        tx.adminKey(.single(Resources.publicKey))
+
+        XCTAssertEqual(tx.adminKey, .single(Resources.publicKey))
+    }
+
+    internal func testGetSetExpirationTime() throws {
+        let tx = ScheduleCreateTransaction()
+        tx.expirationTime(Resources.validStart)
+
+        XCTAssertEqual(tx.expirationTime, Resources.validStart)
+    }
+
+    internal func testGetSetScheduleMemo() throws {
+        let tx = ScheduleCreateTransaction()
+        tx.scheduleMemo(Self.testMemo)
+
+        XCTAssertEqual(tx.scheduleMemo, Self.testMemo)
+    }
+
 }
