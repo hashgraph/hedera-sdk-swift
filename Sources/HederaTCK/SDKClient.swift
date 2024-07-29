833ec1f99ba0c6146108e6b684bcc8725b499a2e
Rob Walworth <robert.walworth@swirldslabs.com>
2024-07-29 16:24:34 -0500

    fix: build issue
    Signed-off-by: Rob Walworth <robert.walworth@swirldslabs.com>

--- Sources/HederaTCK/SDKClient.swift
+++ Sources/HederaTCK/SDKClient.swift
@@ -214,8 +214,8 @@ internal class SDKClient {
             if let maxAutoTokenAssociations = try getOptionalIntParameter(
                 "maxAutoTokenAssociations", params, #function)
             {
-                accountCreateTransaction.maxAutomaticTokenAssociations = UInt32(
-                    truncatingIfNeeded: maxAutoTokenAssociations)
+                accountCreateTransaction.maxAutomaticTokenAssociations = 
+                    Int32(truncatingIfNeeded: maxAutoTokenAssociations)
             }
 
             if let stakedAccountId = try getOptionalStringParameter(
