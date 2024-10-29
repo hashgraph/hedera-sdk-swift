30e88c615c97dee92d469deb650dfdb951d8071a
b-l-u-e <winnie.gitau282@gmail.com>
2024-10-29 03:24:21 +0300

    made some few changes on AddNftAllowance
    Signed-off-by: b-l-u-e <winnie.gitau282@gmail.com>

--- Examples/AddNftAllowance/main.swift
+++ Examples/AddNftAllowance/main.swift
@@ -29,67 +29,75 @@ internal enum Program {
 
         // Step 1: Create an NFT
         let nftCreateReceipt = try await TokenCreateTransaction()
-            .tokenName("HIP-336 NFT1")
-            .tokenSymbol("HIP336NFT1")
-            .tokenType(.nonFungibleUnique)
+            .name("HIP-336 NFT1")
+            .symbol("HIP336NFT1")
+            .tokenType(TokenType.nonFungibleUnique)
             .decimals(0)
             .initialSupply(0)
             .maxSupply(10)
+            .tokenSupplyType(.finite)
             .treasuryAccountId(env.operatorAccountId)
             .adminKey(env.operatorKey.publicKey)
             .supplyKey(env.operatorKey.publicKey)
             .freezeWith(client)
             .execute(client)
             .getReceipt(client)
-        
+
         guard let nftTokenId = nftCreateReceipt.tokenId else {
             fatalError("Failed to create NFT")
         }
-        
+
         print("Created NFT with token ID: \(nftTokenId)")
-        
+
         // Step 2: Mint NFTs
         let cids = [
             "QmNPCiNA3Dsu3K5FxDPMG5Q3fZRwVTg14EXA92uqEeSRXn",
             "QmZ4dgAgt8owvnULxnKxNe8YqpavtVCXmc1Lt2XajFpJs9",
-            "QmPzY5GxevjyfMUF5vEAjtyRoigzWp47MiKAtLBduLMC1T"
+            "QmPzY5GxevjyfMUF5vEAjtyRoigzWp47MiKAtLBduLMC1T",
         ]
-        
+
+        let metadataArray = cids.map {
+            Data($0.utf8)
+        }
+
         for cid in cids {
             let mintReceipt = try await TokenMintTransaction()
                 .tokenId(nftTokenId)
-                .metadata(cid.data(using: .utf8)!)
+                .metadata(metadataArray)
                 .freezeWith(client)
                 .execute(client)
                 .getReceipt(client)
-            
-            guard let serials = mintReceipt.serials else {
-                fatalError("Failed to mint NFT")
+
+            guard let serials = mintReceipt.serials, !serials.isEmpty else {
+                fatalError("Failed to mint NFTs")
             }
-            
-            print("Minted NFT (token ID: \(nftTokenId)) with serial: \(serials.first!)")
+
+            for (index, serial) in serials.enumerated() {
+                print("Minted NFT (token ID: \(nftTokenId)) with serial: \(serials.first!)")
+            }
+
         }
-        
+
         // Step 3: Create spender and receiver accounts
         let spenderKey = PrivateKey.generateEd25519()
         let receiverKey = PrivateKey.generateEd25519()
-        
+
         let spenderAccountId = try await AccountCreateTransaction()
-            .key(spenderKey.publicKey)
+            .key(spenderKey.publicKey.single())
             .initialBalance(Hbar(2))
             .execute(client)
             .getReceipt(client)
             .accountId!
-        
+
         let receiverAccountId = try await AccountCreateTransaction()
-            .key(receiverKey.publicKey)
+            .key(receiverKey.publicKey.single())
             .initialBalance(Hbar(2))
             .execute(client)
             .getReceipt(client)
             .accountId!
-        
+
         print("Created spender account ID: \(spenderAccountId), receiver account ID: \(receiverAccountId)")
-        
+
         // Step 4: Associate spender and receiver with the NFT
         try await TokenAssociateTransaction()
             .accountId(spenderAccountId)
@@ -98,7 +106,7 @@ internal enum Program {
             .sign(spenderKey)
             .execute(client)
             .getReceipt(client)
-        
+
         try await TokenAssociateTransaction()
             .accountId(receiverAccountId)
             .tokenIds([nftTokenId])
@@ -106,47 +114,49 @@ internal enum Program {
             .sign(receiverKey)
             .execute(client)
             .getReceipt(client)
-        
+
         print("Associated spender and receiver accounts with NFT")
-        
+
         // Step 5: Approve NFT allowance for spender
         try await AccountAllowanceApproveTransaction()
             .approveTokenNftAllowance(NftId(tokenId: nftTokenId, serial: 1), env.operatorAccountId, spenderAccountId)
             .approveTokenNftAllowance(NftId(tokenId: nftTokenId, serial: 2), env.operatorAccountId, spenderAccountId)
             .execute(client)
-        
+            .getReceipt(client)
+
         print("Approved NFT allowance for spender")
-        
+
         // Step 6: Transfer NFT using approved allowance
         let transferReceipt = try await TransferTransaction()
-            .addApprovedNftTransfer(NftId(tokenId: nftTokenId, serial: 1), env.operatorAccountId, receiverAccountId)
+            .approvedNftTransfer(NftId(tokenId: nftTokenId, serial: 1), env.operatorAccountId, receiverAccountId)
             .freezeWith(client)
+            .transactionId(TransactionId.generateFrom(spenderAccountId))
             .sign(spenderKey)
             .execute(client)
             .getReceipt(client)
-        
+
         print("Transfer successful with status: \(transferReceipt.status)")
-        
+
         // Step 7: Revoke allowance
-        try await AccountAllowanceDeleteTransaction()
+        _ = try await AccountAllowanceDeleteTransaction()
             .deleteAllTokenNftAllowances(NftId(tokenId: nftTokenId, serial: 2), env.operatorAccountId)
             .execute(client)
-        
+
         print("Revoked NFT allowance")
-        
+
         // Step 8: Attempt transfer after revoking allowance
         do {
             _ = try await TransferTransaction()
-                .addApprovedNftTransfer(NftId(tokenId: nftTokenId, serial: 2), env.operatorAccountId, receiverAccountId)
+                .approvedNftTransfer(NftId(tokenId: nftTokenId, serial: 2), env.operatorAccountId, receiverAccountId)
                 .freezeWith(client)
                 .sign(spenderKey)
                 .execute(client)
-            
+
             print("Transfer after revoking allowance should have failed")
         } catch {
             print("Expected failure: \(error)")
         }
-        
+
         // Cleanup resources by deleting tokens, accounts, etc.
         // Implement cleanup steps...
     }
